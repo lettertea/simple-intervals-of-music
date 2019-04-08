5e7361236ade1a3e8fe4670fb60ea876c7ad703c
nashkenazy <34195263+nashkenazy@users.noreply.github.com>
2019-04-07 18:46:59 -0700

    Add main menu UI

--- app/src/main/java/com/nashkenazy/intervals/MainActivity.java
+++ app/src/main/java/com/nashkenazy/intervals/MainActivity.java
@@ -6,6 +6,8 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.widget.Button;
 import android.os.Handler;
 
@@ -38,6 +40,22 @@ public class MainActivity extends AppCompatActivity implements OnIntervalClick {
         recyclerView.setAdapter(new MyAdapter(INTERVALS, this));
 
         setupRound();
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        if (id == R.id.action_settings) {
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 
     // Adapter listener implementation must be done here instead of setupRound
