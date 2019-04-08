f7f7a7c65c4366d161025c160ae03f84a5779841
nashkenazy <34195263+nashkenazy@users.noreply.github.com>
2019-04-07 19:50:53 -0700

    Create Settings activity with fragment

--- app/src/main/java/com/nashkenazy/intervals/MainActivity.java
+++ app/src/main/java/com/nashkenazy/intervals/MainActivity.java
@@ -1,7 +1,9 @@
 package com.nashkenazy.intervals;
 
 import android.content.Context;
+import android.content.Intent;
 import android.media.MediaPlayer;
+import android.provider.Settings;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
@@ -53,6 +55,8 @@ public class MainActivity extends AppCompatActivity implements OnIntervalClick {
     public boolean onOptionsItemSelected(MenuItem item) {
         int id = item.getItemId();
         if (id == R.id.action_settings) {
+            Intent intent = new Intent(this, SettingsActivity.class);
+            startActivity(intent);
             return true;
         }
         return super.onOptionsItemSelected(item);
