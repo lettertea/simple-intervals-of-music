3d25c9ce09c8833cf71ab9e107cf38018169b0fb
nashkenazy <34195263+nashkenazy@users.noreply.github.com>
2019-04-29 19:03:58 -0700

    Change preference names for fixed note

--- app/src/main/java/com/nashkenazy/intervals/SettingsActivity.java
+++ app/src/main/java/com/nashkenazy/intervals/SettingsActivity.java
@@ -7,8 +7,8 @@ import android.os.Bundle;
 import android.util.Log;
 
 public class SettingsActivity extends AppCompatActivity {
-	public static final String KEY_PREF_FIXED_NOTE = "fixed_note";
-	public static final String KEY_PREF_NOTE_LETTAR = "note_lettar";
+	public static final String KEY_PREF_POSITION = "position";
+	public static final String KEY_PREF_NOTE = "note";
 	public static final String KEY_PREF_OCTAVE = "octave";
 	public static final String KEY_PREF_INCLUDED_INTERVALS = "included_intervals";
 	public static final String KEY_PREF_INTERVAL_TYPE = "interval_type";
@@ -18,6 +18,8 @@ public class SettingsActivity extends AppCompatActivity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		getSupportFragmentManager().beginTransaction().replace(android.R.id.content, new SettingsFragment()).commit();
+
+
 	}
 
 
