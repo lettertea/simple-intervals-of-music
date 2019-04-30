350c17684c5e25d43f655cc54bf190636502f4b4
nashkenazy <34195263+nashkenazy@users.noreply.github.com>
2019-04-29 19:04:25 -0700

    Disable note letter preference depending on the position preference

--- app/src/main/java/com/nashkenazy/intervals/SettingsFragment.java
+++ app/src/main/java/com/nashkenazy/intervals/SettingsFragment.java
@@ -2,6 +2,7 @@ package com.nashkenazy.intervals;
 
 
 import android.os.Bundle;
+import android.util.Log;
 
 import androidx.preference.ListPreference;
 import androidx.preference.Preference;
@@ -14,10 +15,17 @@ public class SettingsFragment extends PreferenceFragmentCompat {
 	public void onCreatePreferences(Bundle savedInstanceState, String rootKey) {
 		setPreferencesFromResource(R.xml.preferences, rootKey);
 
-		ListPreference noteLetter = (ListPreference) findPreference(SettingsActivity.KEY_PREF_NOTE_LETTAR);
-		Preference pref = findPreference(SettingsActivity.KEY_PREF_FIXED_NOTE);
+		ListPreference noteLetter = (ListPreference) findPreference(SettingsActivity.KEY_PREF_NOTE);
+		ListPreference position = (ListPreference) findPreference(SettingsActivity.KEY_PREF_POSITION);
 
-		pref.setOnPreferenceChangeListener((preference, newValue) -> {
+		// Check to disable note letter on initialization
+		if (position.getValue().equals("None")) {
+			noteLetter.setEnabled(false);
+			noteLetter.setValue("-1");  // -1 is the value for "Random"
+		}
+
+		// Check to disable note letter on change
+		position.setOnPreferenceChangeListener((preference, newValue) -> {
 			if (newValue.toString().equals("None")) {
 				noteLetter.setEnabled(false);
 				noteLetter.setValue("-1");  // -1 is the value for "Random"
